e54163502d1abb84b25abd2e49509751c447d04c
Quinten97 <89324378+Quinten97@users.noreply.github.com>
2022-01-18 16:55:56 -0500

    Main Styles added stored main colors in variable format, determined screen sizing/spacing, font/size,

--- src/sections/Contact/Contact.js
+++ src/sections/Contact/Contact.js
@@ -8,8 +8,8 @@ const Contact = () => (
         <strong>Quinten Favorite</strong>
       </p>
       <p>Software Engineer</p>
-      <p>Phone: 1234567890</p>
-      <p>Email: abc@deg.com</p>
+      <p>Phone: 251-266-9861</p>
+      <p>Email: quinten.favorite@outlook.com</p>
     </div>
   </section>
 );
