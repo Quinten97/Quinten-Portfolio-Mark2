d7a3a0654f956ed6e05e7bfa75eecf5a804d0016
Quinten97 <89324378+Quinten97@users.noreply.github.com>
2022-01-19 15:32:46 -0500

    Several style changes just in case thats all i do today

--- src/sections/Introduction/Introduction.js
+++ src/sections/Introduction/Introduction.js
@@ -1,20 +1,26 @@
 import "./Introduction.css";
+import TypeWriter from "react-typewriter";
 
 const Introduction = () => (
   <section id="intro">
-    <p>
+    <div>
       {/* TODO: Give this a big & strong look */}
       <strong>
         Hi! my name is
         {/* TODO: Give the name some swag */}
-        <span>
-          Quinten <br /> <p className="last-name">Favorite</p>
+        <span className="name">
+          <p className="first">Quinten</p> <p className="last">Favorite</p>
         </span>
       </strong>
-      <br />I am a software developer based in Atlanta, Georgia. I love building
-      software using cutting-edge technologies and with best practices and
-      paradigms in the fore-front.
-    </p>
+      <br />
+      <p className="type-writer">
+        <TypeWriter typing={1}>
+          I am a software developer based in Atlanta, Georgia. I love building
+          software using cutting-edge technologies and with best practices and
+          paradigms in the fore-front.
+        </TypeWriter>
+      </p>
+    </div>
     {/* TODO: make them pretty */}
     <div className="buttons-container">
       <a href="#portfolio" className="primary">
