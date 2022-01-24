ea161a73035bcbe679041e622cc085ca610ba3f0
Quinten97 <89324378+Quinten97@users.noreply.github.com>
2022-01-24 12:53:35 -0500

    cards for projects

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -3,14 +3,17 @@ import "./Project.css";
 // TODO: make this componet use the props to generate a nice project display
 const Project = ({ title, img, description, preview, repo }) => (
   <article>
+    <img src={img} alt="project thumbnail" />
     <p>{title}</p>
     <p>{description}</p>
-    <a href={repo} target="_blank">
-      :eye: view source
-    </a>
-    <a href={preview} target="_blank">
-      <img src={img} alt="project thumbnail" />
-    </a>
+    <div className="button-container">
+      <a className="source-button" href={repo} target="_blank">
+        👁 View Source
+      </a>
+      <a className="demo-button" href={preview} target="_blank">
+        Demo
+      </a>
+    </div>
   </article>
 );
 
