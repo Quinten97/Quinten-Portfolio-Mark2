717944e21cb3ebd9fe482b170cf4b42e70961b08
Quinten97 <89324378+Quinten97@users.noreply.github.com>
2022-01-24 10:03:20 -0500

    added portfolio section added projects to portfolio

--- src/sections/Portfolio/Portfolio.js
+++ src/sections/Portfolio/Portfolio.js
@@ -1,4 +1,8 @@
 import "./Portfolio.css";
+import heatmap from "./thumbnail-photos/HeatmapThumb.JPG";
+import joke from "./thumbnail-photos/JokeThumb.JPG";
+import counter from "./thumbnail-photos/MTGCounterThumb.JPG";
+import clicker from "./thumbnail-photos/SpaceClickerThumb.JPG";
 
 const Portfolio = () => (
   <section id="portfolio">
@@ -9,7 +13,32 @@ const Portfolio = () => (
       provide additional information.
     </p>
     {/* Show a project component below */}
-    <div className="projects-container"></div>
+    <div className="projects-container">
+      <h4>Rocketship Clicker</h4>
+      <button>
+        <a href="https://rocketship-clicker.netlify.app/" target="_blank">
+          <img src={clicker} alt="thumbnail of app" />
+        </a>
+      </button>
+      <h4>Get a Joke!</h4>
+      <button>
+        <a href="https://get-joke.netlify.app/" target="_blank">
+          <img src={joke} alt="thumbnail of app" />
+        </a>
+      </button>
+      <h4>Two Player Point Tracker</h4>
+      <button>
+        <a href="https://magiccounter.netlify.app/" target="_blank">
+          <img src={counter} alt="thumbnail of app" />
+        </a>
+      </button>
+      <h4>Monthly Global Land-Surface Temp.</h4>
+      <button>
+        <a href="https://codepen.io/Quinten97/full/oNemdmy" target="_blank">
+          <img src={heatmap} alt="thumbnail of app" />
+        </a>
+      </button>
+    </div>
   </section>
 );
 
