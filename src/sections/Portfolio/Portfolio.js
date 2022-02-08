817a733250a500e8862aa96b0c4b031eb176337a
Quinten97 <89324378+Quinten97@users.noreply.github.com>
2022-02-07 23:04:43 -0500

    Fixed source button urls

--- src/sections/Portfolio/Portfolio.js
+++ src/sections/Portfolio/Portfolio.js
@@ -20,28 +20,28 @@ const Portfolio = () => (
         img="https://i.imgur.com/PIPIUME.jpg"
         description="an idle clicker game programmed in React."
         preview="https://rocketship-clicker.netlify.app/"
-        repo=""
+        repo="https://github.com/Quinten97/Space-Clicker-ReactGame"
       />
       <Project
         title="Get a Joke!"
         img="https://i.imgur.com/0RAWzfi.jpg"
         description="A simple app that generates a joke from an API"
         preview="https://get-joke.netlify.app/"
-        repo=""
+        repo="https://github.com/Quinten97/Quintens-Joke-Generator"
       />
       <Project
         title="Two Player Point Tracker"
         img="https://i.imgur.com/occJaZZ.jpg"
         description="A two player point tracker"
         preview="https://magiccounter.netlify.app/"
-        repo=""
+        repo="https://github.com/Quinten97/magic-counter"
       />
       <Project
         title="Monthly Global Land-Surface Temp"
         img="https://i.imgur.com/w3pPcd8.jpg"
         description="A heatmap made with d3.js"
         preview="https://codepen.io/Quinten97/full/oNemdmy"
-        repo=""
+        repo="https://github.com/Quinten97/D3-Heatmap-FCCProject"
       />
     </div>
   </section>
